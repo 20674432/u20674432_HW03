eddb990f3879bd2ad71d70288b2c6e7687257532
Thapelo <Thapelo@THAPELO>
2022-06-08 20:37:21 +0200

    Add project files.

---
+++ HW3/Models/FileModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW3.Models
+{
+    public class FileModel
+    {
+        public string FileName { get; set; }
+       /* public FileModel( string FileName)
+        {
+            FileName = this.FileName;   
+        }*/
+    }
+}
